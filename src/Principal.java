c751ff2c488833563cf118665fee631673780ff0
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2023-10-04 14:42:14 -0300

    subindo um api

--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Principal {
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://imdb-top-100-movies.p.rapidapi.com/top2"))
+                .header("X-RapidAPI-Key", "5c2dea093fmshd8d3b6dd33c4ec7p18c707jsnf9a69d383cd3")
+                .header("X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
 }
